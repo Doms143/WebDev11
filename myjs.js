b74dbc5229847023164ff9864a37e0c987dce6c9
Doms143 <113317460+Doms143@users.noreply.github.com>
2022-10-03 08:48:15 +0800

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,26 @@
+// Take use input
+var x = window.prompt("Enter first number: ");
+var y = window.prompt("Enter second number: ");
+
+// Convert string to integer
+var num1 = parseInt(x);
+console.log('First input numer: ' + num1)
+
+var num2 = parseInt(y);
+console.log('Second input numer: ' + num2)
+
+// Calculate the Sum
+var sum =  num1 + num2;
+var difference = num1 - num2;
+var product = num1 * num2;
+var qoutient = num1 / num2;
+
+// Show the results
+console.log('The sum of ' + num1 + ' + ' + num2 + ' is: ' + sum);
+console.log('The difference of ' + num1 + ' - ' + num2 + ' is: ' + difference);
+console.log('The product of ' + num1 + ' * ' + num2 + ' is: ' + product);
+console.log('The qoutient of ' + num1 + ' / ' + num2 + ' is: ' + qoutient);
+
+
+// sa 1/8 butangi`g Name, date, JS Math
+//output should include sum, difference, product, qoutient
