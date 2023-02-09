29728c6ef687b37c181429551b5d8adec1e868bb
Doms143 <113317460+Doms143@users.noreply.github.com>
2023-02-09 23:54:07 +0800

    Update ChilliToGoTacatani.java

--- ChilliToGoTacatani.java
+++ ChilliToGoTacatani.java
@@ -14,19 +14,14 @@ public class ChilliToGoTacatani
       
       cost1 = child * childPrice;
       cost2 = adult * adultPrice;
-      total = cost1 + cost2;
-      profit1 = 4.35 * adult;
-      profit2 = 3.10 * child;
-      totalProfit1 = cost2 - profit1;
-      totalProfit2 = cost1 - profit2;
+      //*total = cost1 + cost2;
+      profit1 = 3.10 * child;
+      profit2 = 4.35 * adult;
+      totalProfit1 = cost2 - profit2;
+      totalProfit2 = cost1 - profit1;
       profit = totalProfit1 + totalProfit2;
       
-      System.out.printf("The total Profit is $%.2f and the total amount to pay is $%.2f",profit ,total);
-      
-      
-   
-   
-   
+      System.out.printf("The total money collected from adult meal is $%.2f and the total money collected from child meal is $%.2f. \nThe adult meal profit is $%.2f and the child meal profit is $%.2f. The total grand profit is $%.2f.",cost2 ,cost1 ,totalProfit1 ,totalProfit2, profit);
    }
 
 
